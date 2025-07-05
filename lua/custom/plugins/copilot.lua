51f0d48e00e21320b85b94babe11e27a99680938
Geoc2022 <geoc2022@gmail.com>
2025-07-04 23:31:06 -0400

    Add sessions and ad file dir

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,13 +1,13 @@
 return {
-	"github/copilot.vim",
-	init = function()
-		vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-		  expr = true,
-		  replace_keycodes = false
-		})
-		vim.g.copilot_no_tab_map = true
-		vim.b.copilot_enabled = false
-	end
+	-- "github/copilot.vim",
+	-- init = function()
+	-- 	vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+	-- 	  expr = true,
+	-- 	  replace_keycodes = false
+	-- 	})
+	-- 	vim.g.copilot_no_tab_map = true
+	-- 	vim.b.copilot_enabled = false
+	-- end
 	--
 	-- "zbirenbaum/copilot-cmp",
 	-- cmd = "Copilot",
@@ -15,4 +15,29 @@ return {
 	-- config = function()
 	--   require("copilot").setup({})
 	-- end,
+	"zbirenbaum/copilot.lua",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+	require("copilot").setup({
+		suggestion = {
+			enabled = true,
+			auto_trigger = true,
+			hide_during_completion = false,
+			debounce = 75,
+			trigger_on_accept = true,
+			keymap = {
+			  accept = "<C-J>",
+			  accept_word = false,
+			  accept_line = false,
+			  next = "<M-]>",
+			  prev = "<M-[>",
+			  dismiss = "<C-]>",
+			},
+		},
+		filetypes = {
+			markdown = true
+		},
+	})
+	end,
 }
