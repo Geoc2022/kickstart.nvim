4e3494ce333d2503b7caf3d2ff639f33092e1458
Geoc2022 <geoc2022@gmail.com>
2026-03-01 16:19:08 -0500

    Update plugins

--- lua/custom/plugins/atone.lua
+++ lua/custom/plugins/atone.lua
@@ -2,4 +2,7 @@ return {
   'XXiaoA/atone.nvim',
   cmd = 'Atone',
   opts = {}, -- your configuration here
+  keys = {
+    { '<leader>at', '<cmd>Atone toggle<CR>', desc = 'Atone' },
+  },
 }
--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,30 +1,30 @@
 return {
-  'fzdwx/bufline.nvim',
-  event = 'BufEnter',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-  },
-  config = function()
-    local buf = require 'bufline'
-    buf.setup {
-      separator = function()
-        return {
-          str = '/',
-          hl = '',
-        }
-      end,
-      folder = function()
-        return {
-          str = '',
-          hl = 'BufLineFolder',
-        }
-      end,
-      dirName = function()
-        return {
-          str = '',
-          hl = 'BufLineDirName',
-        }
-      end,
-    }
-  end,
+  -- 'fzdwx/bufline.nvim',
+  -- event = 'BufEnter',
+  -- dependencies = {
+  --   'nvim-tree/nvim-web-devicons',
+  -- },
+  -- config = function()
+  --   local buf = require 'bufline'
+  --   buf.setup {
+  --     separator = function()
+  --       return {
+  --         str = '/',
+  --         hl = '',
+  --       }
+  --     end,
+  --     folder = function()
+  --       return {
+  --         str = '',
+  --         hl = 'BufLineFolder',
+  --       }
+  --     end,
+  --     dirName = function()
+  --       return {
+  --         str = '',
+  --         hl = 'BufLineDirName',
+  --       }
+  --     end,
+  --   }
+  -- end,
 }
